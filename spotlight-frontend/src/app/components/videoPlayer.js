847cf9b1745c0090e1ce63a7a9b19684482c42a0
Roman Sinkus <sinkusroman@gmail.com>
2025-01-19 04:29:34 -0800

    Connected backend to frontend and made UI changes including the following: * typing animation * border radius * information underneath video output

--- spotlight-frontend/src/app/components/videoPlayer.js
+++ spotlight-frontend/src/app/components/videoPlayer.js
@@ -4,7 +4,7 @@ import './../globals.css'; // Import your styles
 const VideoPlayer = ({ playbackId }) => {
     return (
         <iframe
-            className="w-full h-128 border-none rounded-lg mb-24 flex-grow"
+            className="w-full h-128 border-none rounded-lg mb-10 flex-grow"
             src={`https://lvpr.tv?v=${playbackId}`}
             allowFullScreen
             allow="autoplay; encrypted-media; fullscreen; picture-in-picture"
