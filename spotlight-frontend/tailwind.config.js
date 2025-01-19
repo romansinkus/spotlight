d5d637dffc02ea631e4fb168d5e3fc651abefed9
madoka <mnogaki@my.bcit.ca>
2025-01-18 22:29:33 -0800

    browse page layout

--- spotlight-frontend/tailwind.config.js
+++ spotlight-frontend/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}',],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#E2E2E2',
+        'welcome-colour': '#FBFF86',
+        'progress-bar': '#B99FCE',
+        'filter': '#B0D4B0',
+        'white': '#ffffff',
+      },
+    },
   },
   plugins: [],
 }
